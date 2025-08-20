d034d114ffafea98553c0be4639c83fdd2b8bb69
bharathkumarg729-cell <bharathkumarg729@gmail.com>
2025-08-20 20:37:34 +0530

    Add files via upload

---
+++ FrequencyCount.java
@@ -0,0 +1,20 @@
+package logicalBuilding1;
+
+public class FrequencyCount {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,3,3,3,4};
+        boolean[] visited = new boolean[arr.length];
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (visited[i]) continue;
+            int count = 1;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            System.out.println(arr[i] + " -> " + count);
+        }
+    }
+}
